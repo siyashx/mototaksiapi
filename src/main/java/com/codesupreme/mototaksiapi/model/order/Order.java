4d4cfebc32933352e766136f54bbf7c98e0b2c42
siyashx <siyashxyusifov@gmail.com>
2025-07-17 02:48:06 +0400

    .

--- src/main/java/com/codesupreme/mototaksiapi/model/order/Order.java
+++ src/main/java/com/codesupreme/mototaksiapi/model/order/Order.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 import java.util.List;
@@ -24,19 +25,30 @@ public class Order {
 
     private Long customerId;
     private Long courierId;
+
     @ElementCollection
+    @CollectionTable(name = "order_from_address", joinColumns = @JoinColumn(name = "order_id"))
+    @Column(name = "from_address")
     private List<String> fromAddress;
 
     @ElementCollection
+    @CollectionTable(name = "order_to_address", joinColumns = @JoinColumn(name = "order_id"))
+    @Column(name = "to_address")
     private List<String> toAddress;
 
     @ElementCollection
+    @CollectionTable(name = "order_cancelled_couriers", joinColumns = @JoinColumn(name = "order_id"))
+    @Column(name = "cancelled_courier_id")
     private List<Long> cancelledCourierIds;
 
     private String status;
     private Double price;
     private Double distance;
+
     @JsonProperty("isDisable")
     private Boolean isDisable;
+
+    @CreationTimestamp
     private Date createdAt;
 }
+
